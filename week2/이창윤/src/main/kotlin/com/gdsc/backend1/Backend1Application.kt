defe422ecf32fae8f4ad349514dbe5424dce101f
yun8565 <yunyun8565@naver.com>
2022-10-13 00:51:34 +0900

    Feat: 프로젝트 생성
    Spring Initializr 사용

---
+++ week2/이창윤/src/main/kotlin/com/gdsc/backend1/Backend1Application.kt
@@ -0,0 +1,11 @@
+package com.gdsc.backend1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Backend1Application
+
+fun main(args: Array<String>) {
+	runApplication<Backend1Application>(*args)
+}
